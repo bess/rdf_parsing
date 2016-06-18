56212f5c2b21c29de25b3d26c5210a2a7602fbbe
Bess Sadler <bess@stanford.edu>
2016-06-17 17:28:49 -0700

    Load files via http, load multiple files into the same RDF::Repository object, make a Gemfile

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+ruby '2.2.2'
+
+gem 'sparql'
+gem 'sparql-client'
+gem 'openssl'
+gem 'rdf'
+gem 'rdf-raptor'
+gem 'rdf-json'
+gem 'rdf-trix'
--- parse_from_file.rb
+++ parse_from_file.rb
@@ -3,6 +3,8 @@
 require 'rdf'
 require 'rdf/raptor'
 require 'sparql'
+require 'net/http'
+require 'openssl'
 
 # RDF::Reader.open("foaf_files/foaf.rdf") do |reader|
 #   reader.each_statement do |statement|
@@ -20,7 +22,14 @@ SELECT *
   WHERE { ?s foaf:knows ?o }
 "
 
-queryable = RDF::Repository.load("foaf_files/foaf.rdf")
+# queryable = RDF::Repository.load("foaf_files/foaf.rdf")
+bess_rdf = RDF::Resource(RDF::URI.new("http://stanford.edu/~bess/foaf.rdf"))
+hacker_nt = RDF::Resource(RDF::URI.new("http://datagraph.org/jhacker/foaf.nt"))
+
+# Load multiple RDF files into the same RDF::Repository object
+queryable = RDF::Repository.load(hacker_nt)
+queryable.load(bess_rdf)
+
 sse = SPARQL.parse(query)
 sse.execute(queryable) do |result|
   puts result.o
